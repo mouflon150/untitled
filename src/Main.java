fe0e106be51b02b6998a74b3935d466d6495dc62
Marlen Temirbaev <mouflon150@gmail.com>
2022-11-12 09:22:31 +0600

    calc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        char operation = scanner.next().charAt(0);
+        switch (operation) {
+            case '+':
+                System.out.println(plus(firstNumber,secondNumber));
+                break;
+            case '-':
+                System.out.println(minus(firstNumber,secondNumber));
+                break;
+        }
 
+
+
+    }
+    public static int plus (int a, int b) {
+        return a + b;
+    }
+    public static int minus (int a, int b) {
+        return  a - b;
     }
 }
