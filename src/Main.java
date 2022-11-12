9daae9b6f88f50ea8a6d16234641c4732117537f
Neon64F <Neon101110@gmail.com>
2022-11-12 09:29:08 +0600

    my prohect

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        hello();
+        welcome();
+    }
+    static void hello(){
+        System.out.println("Word my");
+    }
+    static void welcome(){
+        System.out.println("It's My world");
+    }
 }
