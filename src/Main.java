6ba6adc7faf1e7380bbb97f87d592e86aa63f830
Marlen Temirbaev <mouflon150@gmail.com>
2022-11-13 18:37:25 +0600

    calcUpdate

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[] strings = new String[]{"Введите первое число: ", "Введите второе число: ",
+                "Выберите операцию: " + "\n".concat("""
+                        - прибавить (+);
+                        - отнять (-);
+                        - умножить (*);
+                        - разделить (/)."""), "Результат: "};
         Scanner scanner = new Scanner(System.in);
+        System.out.println(strings[0]);
         double firstNumber = scanner.nextDouble();
+        System.out.println(strings[1]);
         double secondNumber = scanner.nextDouble();
+        System.out.println(strings[2]);
         char operation = scanner.next().charAt(0);
         switch (operation) {
-            case '+' -> System.out.println(plus(firstNumber, secondNumber));
-            case '-' -> System.out.println(minus(firstNumber, secondNumber));
-            case '*' -> System.out.println(multiplication(firstNumber, secondNumber));
-            case '/' -> System.out.println(division(firstNumber, secondNumber));
+            case '+' -> System.out.println(strings[3] + plus(firstNumber, secondNumber));
+            case '-' -> System.out.println(strings[3] + minus(firstNumber, secondNumber));
+            case '*' -> System.out.println(strings[3] + multiplication(firstNumber, secondNumber));
+            case '/' -> System.out.println(strings[3] + division(firstNumber, secondNumber));
             default -> System.out.println("Неправильная операция!");
+
         }
-
-
     }
 
     public static double plus(double firstNumber, double secondNumber) {
