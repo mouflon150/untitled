2d4d59e66292ce01f93ab0927144549054d72dbb
Marlen Temirbaev <mouflon150@gmail.com>
2022-11-19 14:39:04 +0600

    Commit message

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,20 +8,24 @@ public class Calculator {
                         - отнять (-);
                         - умножить (*);
                         - разделить (/)."""), "Результат: "};
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(strings[0]);
-        double firstNumber = scanner.nextDouble();
-        System.out.println(strings[1]);
-        double secondNumber = scanner.nextDouble();
-        System.out.println(strings[2]);
-        char operation = scanner.next().charAt(0);
-        switch (operation) {
-            case '+' -> System.out.println(strings[3] + plus(firstNumber, secondNumber));
-            case '-' -> System.out.println(strings[3] + minus(firstNumber, secondNumber));
-            case '*' -> System.out.println(strings[3] + multiplication(firstNumber, secondNumber));
-            case '/' -> System.out.println(strings[3] + division(firstNumber, secondNumber));
-            default -> System.out.println("Неправильная операция!");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(strings[0]);
+            double firstNumber = scanner.nextDouble();
+            System.out.println(strings[1]);
+            double secondNumber = scanner.nextDouble();
+            System.out.println(strings[2]);
+            char operation = scanner.next().charAt(0);
+            switch (operation) {
+                case '+' -> System.out.println(strings[3] + plus(firstNumber, secondNumber));
+                case '-' -> System.out.println(strings[3] + minus(firstNumber, secondNumber));
+                case '*' -> System.out.println(strings[3] + multiplication(firstNumber, secondNumber));
+                case '/' -> System.out.println(strings[3] + division(firstNumber, secondNumber));
+                default -> System.out.println("Неправильная операция!");
 
+            }
+        } catch (Exception exception) {
+            System.out.println("Программа завершена.");
         }
     }
 
@@ -42,7 +46,6 @@ public class Calculator {
             System.out.println("На нуль делить нельзя!");
         }
         return firstNumber / secondNumber;
-
 
     }
 }
