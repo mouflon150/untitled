91f2fc51819391a20974156ab5a33ae5d177a2d9
Marlen Temirbaev <mouflon150@gmail.com>
2022-12-28 18:51:54 +0600

    Commit message

---
+++ src/collections/SetAndHashSet/Main.java
@@ -0,0 +1,28 @@
+package collections.SetAndHashSet;
+
+import java.util.*;
+
+public class Main {
+
+    public static int getSum(List<Integer> nums) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        List<Integer> ints = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            int randomNums = random.nextInt(1, 400);
+            ints.add(randomNums);
+        }
+        System.out.println("Sum of all numbers: " + getSum(ints));
+
+        Set<Integer> set = new HashSet<>(ints);
+        set.forEach(System.out::println);
+    }
+}
