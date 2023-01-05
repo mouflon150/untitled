9416b7d765f60d6bfa8b35d1625cc600697ed895
Marlen Temirbaev <mouflon150@gmail.com>
2023-01-05 12:48:51 +0600

    Commit message

--- src/task4/SymmetricDifference.java
+++ src/task4/SymmetricDifference.java
@@ -9,6 +9,7 @@ public class SymmetricDifference {
 
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
+
         set1.add(1);
         set1.add(2);
         set1.add(3);
